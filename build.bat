9743655f004d9d79bb4ac7b563a5e7636b2f0452
Kacper Grzelak <upsetboii@gmail.com>
2025-05-21 17:49:24 +0200

    version for antek

---
+++ build.bat
@@ -0,0 +1,2 @@
+gcc main.c -Iinclude -Llib -lSDL3 -o build/gra.exe
+pause
--- main.c
+++ main.c
@@ -1,10 +1,8 @@
+#define SDL_ENABLE_OLD_NAMES
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_render.h>
 #include <stdbool.h>
 #include <stdio.h>
-
-#define SDL_NO_OLDNAMES
-
 
 struct Puzzle
 {
@@ -12,72 +10,83 @@ struct Puzzle
     int actualPosition;
     int startPosition;
     int destination;
-    //image ;  do wyszukania
+    // image ;  do wyszukania
 };
 
 struct Board
 {
     int widthOfBoard;
     int heightOfBoard;
-    //int TableOfPositions[heightOfBoard][widthOfBoard][2];
-    //image
+    // int TableOfPositions[heightOfBoard][widthOfBoard][2];
+    // image
 };
 
 /*struct resetButton
 {
-    
+
 };*/
 
-//rysowanie przycisków na oknie wejsciowym
+// rysowanie przycisków na oknie wejsciowym
 
 const SDL_FRect buttonRect = {150.0f, 120.0f, 100.0f, 50.0f};
 
-void DrawButton(SDL_Renderer *renderer, SDL_FRect *rect, bool hovered, bool pressed) {
-    if (pressed || hovered) {
+void DrawButton(SDL_Renderer *renderer, SDL_FRect *rect, bool hovered, bool pressed)
+{
+    if (pressed || hovered)
+    {
         SDL_SetRenderDrawColor(renderer, 30, 19, 237, 200);
-    } else {
+    }
+    else
+    {
         SDL_SetRenderDrawColor(renderer, 30, 19, 237, 200);
     }
 
-    SDL_RenderFillRect(renderer, rect); // te dwie chat trzy razu chyba zmineniał ale nadal nie jestem pewien czy są dobrze bo warningi cały czas sie wyswietlają
+    SDL_RenderFillRectF(renderer, rect); // te dwie chat trzy razu chyba zmineniał ale nadal nie jestem pewien czy są dobrze bo warningi cały czas sie wyswietlają
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderDrawRect(renderer, rect); //
+    SDL_RenderDrawRectF(renderer, rect); //
 }
 
-
-int main(void) {
+int main(void)
+{
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *win = SDL_CreateWindow("Puzzle", 800, 600, 0);
-    if(!win){
+    if (!win)
+    {
         SDL_Log("Failed to make a window: %s", SDL_GetError());
         SDL_Quit();
         return -1;
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(win, NULL);
-    if(!renderer){
+    if (!renderer)
+    {
         SDL_Log("Failed to make renderer %s", SDL_GetError());
         SDL_Quit();
         return -2;
     }
-    
-    bool gameRunning=true;
-    while(gameRunning){
+
+    bool gameRunning = true;
+    while (gameRunning)
+    {
         SDL_Event event;
-        while(SDL_PollEvent(&event)){
+        while (SDL_PollEvent(&event))
+        {
             switch (event.type)
             {
-                case SDL_EVENT_QUIT:
-                    gameRunning=false;
-                    break;
+            case SDL_EVENT_QUIT:
+                gameRunning = false;
+                break;
             }
         }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // czarne tło
         SDL_RenderClear(renderer);
-        SDL_Log("Cleared");
-        DrawButton(renderer, &buttonRect, false, false);
+
+        DrawButton(renderer, &buttonRect, false, false); // przycisk
+
         SDL_RenderPresent(renderer);
     }
-    SDL_Log("SDL version: %d.%d.%d", SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_PATCHLEVEL);//logów nie widać w terminalu
+    SDL_Log("SDL version: %d.%d.%d", SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_PATCHLEVEL); // logów nie widać w terminalu
     SDL_Delay(2000);
     SDL_DestroyWindow(win);
     SDL_DestroyRenderer(renderer);
